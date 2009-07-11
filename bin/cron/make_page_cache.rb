7e29e0397eb91624ad208fb06369513c1c59c0d3
sasata299 <sasata299@livedoor.com>
2009-07-12 08:33:07 +0900

    add cron

---
+++ bin/cron/make_page_cache.rb
@@ -0,0 +1,22 @@
+#!/usr/bin/ruby
+
+ENV["RAILS_ENV"] ||= 'production'
+
+require 'rubygems'
+require 'active_record'
+$:.push(File.dirname(__FILE__) + "/../../app/models")
+require 'movie'
+
+config = YAML.load_file(File.dirname(__FILE__) + "/../../config/database.yml")
+ActiveRecord::Base.establish_connection(config[ ENV["RAILS_ENV"] ])
+ActiveRecord::Base.logger=Logger.new(STDOUT)
+
+$KCODE = 'u'
+
+movies = Movie.find(:all)
+movies.each do |movie|
+  url_1 = "http://netaru.sasata299.com/movie/#{movie.id}"
+  url_2 = "http://netaru.sasata299.com/netabares/show/#{movie.id}"
+  system "wget -O /dev/null #{url_1}"
+  system "wget -O /dev/null #{url_2}"
+end
